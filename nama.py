3a9bf3fb01b5291ccaae3ac084cd47cef44ace64
Irwansyah <109840077+iwplus@users.noreply.github.com>
2022-07-24 08:13:53 +0800

    Update nama.py

--- nama.py
+++ nama.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 27 07:32:49 2021
-
-@author: LENOVO
-"""
 import pandas as pd
 import numpy as np
 import os
 #import fnmatch
 
-files = pd.read_csv('e:/2021/KNMIPA 2021/Koreksi Wilayah/wil16.csv')
+files = pd.read_csv('e:/2021/KNMIPA 2021/Koreksi Wilayah/wil16.csv') ### you can change to your desired directory
 
 files = np.array(files)
 #print(files)
@@ -18,10 +12,10 @@ files = np.array(files)
 for i in range(files.shape[0]):
     str1 = str(files[i,0])
     #print(str1)
-    for filename in os.listdir('e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16'):
+    for filename in os.listdir('e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16'): ### you can change to your desired directory
         if filename.startswith(str1):
-            nama = 'e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16/'+str(files[i,1])+'_'+files[i,2]+'_'+str1+'_U4.pdf'
-            old_name = 'e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16/'+ filename
+            nama = 'e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16/'+str(files[i,1])+'_'+files[i,2]+'_'+str1+'_U4.pdf' ### you can change according to your desired directory
+            old_name = 'e:/2021/KNMIPA 2021/Koreksi Wilayah/Wilayah 16/'+ filename ### you can change according to your desired directory
             print(filename)
             print(nama)
             print('-----')
